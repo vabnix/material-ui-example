bd26487d1e9d60b2da72322014e3fe6a6ef16fb6
vabnix <vabnix@hotmail.com>
2018-11-13 21:53:08 -0600

    first commit

---
+++ src/component/nav/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+class Footer extends Component {
+    render(){
+        return(
+            <footer class="navbar-default navbar-fixed-bottom">
+                <div class="container-fluid">
+                    <span>Winning!</span>
+                </div>
+            </footer>
+        );
+
+    }
+
+}
+
+export default Footer;
